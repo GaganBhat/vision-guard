395549b3183097aa937bf448b9b0ea73e76c5def
Gagan Bhat <gaganbhat6@gmail.com>
2020-05-02 11:39:26 -0500

    Working face indexed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
-import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.*;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamException;
 
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.util.List;
 
 
 public class Main {
@@ -21,6 +22,34 @@ public class Main {
 		AmazonRekognition amazonRekognition = AmazonRekognitionClientBuilder.defaultClient();
 
 		Image testImage = new Image().withBytes(getByteBuffer("testpic.jpg"));
+
+		IndexFacesRequest indexFacesRequest = new IndexFacesRequest()
+				.withImage(testImage)
+				.withQualityFilter(QualityFilter.AUTO)
+				.withMaxFaces(1)
+				.withCollectionId(collectionID)
+				.withExternalImageId("testpic.jpg")
+				.withDetectionAttributes("DEFAULT");
+
+		IndexFacesResult indexFacesResult = amazonRekognition.indexFaces(indexFacesRequest);
+
+		System.out.println("Results for " + "testpic.jpg");
+		System.out.println("Faces indexed:");
+		List<FaceRecord> faceRecords = indexFacesResult.getFaceRecords();
+		for (FaceRecord faceRecord : faceRecords) {
+			System.out.println("  Face ID: " + faceRecord.getFace().getFaceId());
+			System.out.println("  Location:" + faceRecord.getFaceDetail().getBoundingBox().toString());
+		}
+
+		List<UnindexedFace> unindexedFaces = indexFacesResult.getUnindexedFaces();
+		System.out.println("Faces not indexed:");
+		for (UnindexedFace unindexedFace : unindexedFaces) {
+			System.out.println("  Location:" + unindexedFace.getFaceDetail().getBoundingBox().toString());
+			System.out.println("  Reasons:");
+			for (String reason : unindexedFace.getReasons()) {
+				System.out.println("   " + reason);
+			}
+		}
 	}
 
 	public static ByteBuffer getByteBuffer(String fileName) throws IOException {
